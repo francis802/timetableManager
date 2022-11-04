37f70e2c597f331040d9b684a6a09b65aed21285
Tomás Gaspar <tomasc.gaspar@hotmail.com>
2022-11-04 12:41:21 +0000

    changed gen latex in dconfig to no

--- main.cpp
+++ main.cpp
@@ -6,39 +6,11 @@
 using namespace std;
 
 int main() {
+    ofstream out("../log_failed_changes.txt");
+    out.close();
     GestaoHor gestao = GestaoHor();
     gestao.getDataStudent("../students_classes.csv", "../classes.csv");
     Menu menu = Menu(gestao);
     menu.start();
-
-
-    /*
-    list<UCTurma> turmas = gestao.getDataAula("../classes.csv");
-    for (UCTurma uc: turmas){
-        cout << uc.getCodUc() << '|' << uc.getCodTurma() << '\n';
-        for (Aula a: uc.getTimetable()){
-            cout << '\t' << a.getWeekday();
-            cout << ' ' << a.getStartHour();
-            cout << ' ' << a.getDuration();
-            cout << ' ' << a.getType() << '\n';
-        }
-    }
-
-    cout << "----------------------------------------------------\n----------------------------------------------------\n";
-
-    gestao.getDataStudent("../students_classes.csv","../classes.csv");
-    for (Estudante e : gestao.getStudents()) {
-        cout << e.getName() << '\n';
-        for (UCTurma turma : e.getTurmas()){
-            cout << turma.getCodUc() << " -- " << turma.getCodTurma() << '\n';
-            for (Aula a : turma.getTimetable()){
-                cout << '\t' << a.getWeekday();
-                cout << ' ' << a.getStartHour();
-                cout << ' ' << a.getDuration();
-                cout << ' ' << a.getType() << '\n';
-            }
-        }
-    }
-     */
     return 0;
 }
