f147d5a6a13e05e4c702ce007e8e1125700bb3ba
Francisco Campos <franciscosccampos@gmail.com>
2022-11-06 11:48:30 +0000

    File Path Fixed

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main() {
     ofstream out("../log_failed_changes.txt");
     out.close();
     GestaoHor gestao = GestaoHor();
-    gestao.getDataStudent("../students_classes.csv", "../classes.csv");
+    gestao.getDataStudent("../ficheiros/csv/students_classes.csv", "../ficheiros/csv/classes.csv");
     Menu menu = Menu(gestao);
     menu.start();
     return 0;
